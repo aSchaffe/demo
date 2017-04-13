108f73b46867ec44b7128cd43b2cc741ce593331
aSchaffe <info@axel-schaffrath.de>
2017-04-13 09:12:36 +0200

    modifed task runner

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,23 @@
 var gulp = require('gulp');
 var changed = require('gulp-changed'),
-    jshint = require ('gulp-jshint'),
-    concat = require ('gulp-concat'),
-    uglify = require ('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
-    imagemin = require ('gulp-imagemin'),
+    imagemin = require('gulp-imagemin'),
     clean = require('gulp-clean'),
-    minifyhtml = require ('gulp-minify-html'),
-    autoprefixer = require ('gulp-autoprefixer'),
-    minifyCSS = require ('gulp-minify-css');
+    less = require('gulp-less'),
+    path = require('path'),
+    watch = require('gulp-watch'),
+    cleanCSS = require('gulp-clean-css'),
+    sourcemaps = require('gulp-sourcemaps'),
+    minifyhtml = require('gulp-minify-html'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css');
 
-gulp.task('images', function() {
-    var imgSrc = 'files/img/**/*',
-        imgDst = 'files/img';
 
-    gulp.src(imgSrc)
-        .pipe(changed(imgDst))
-        .pipe(imagemin())
-        .pipe(gulp.dest(imgDst));
-});
-
-gulp.task('js', function() {
+gulp.task('js', function () {
     return gulp.src(['gulpfile.js', 'assets/js/**/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
-});
-
-gulp.task('watch', function () {
-    watcherJS.on('changed', function (event) {
-        console.log('File ' + event.path + ' was ' + event.type +
-            ', running tasks...');
-    });
 });
